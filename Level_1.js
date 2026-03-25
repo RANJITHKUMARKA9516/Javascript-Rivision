a4d0e1acedad9ecc5badbbd8d0aad3627a60d1c1
RANJITHKUMARKA9516 <158045839+RANJITHKUMARKA9516@users.noreply.github.com>
2026-03-25 21:23:36 +0530

    Javascript Rivision Level 1

---
+++ Level_1.js
@@ -0,0 +1,10 @@
+// ! What is Java script ?
+//? JavaScript is a programming language used to make websites interactive and dynamic.
+
+//! What is Javascipt Engine 
+//? A JavaScript Engine is a program that runs JavaScript code.                               👉 It takes your JavaScript code and converts it into machine code so the computer can understand and execute it.
+
+//! Client side vs server side
+//?Client-side refers to code that runs in the user’s browser and is responsible for user interface and interaction.
+
+//? Server-side refers to code that runs on a server and is responsible for processing data, business logic, and database operations.
